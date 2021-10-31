579efef85d3dcd5e68392ff8355bdbdc11f57bc3
Your Name <quytvgcd17298@fpt.edu.vn>
2021-10-31 22:29:33 +0700

    Insert into SQLite database

--- app/src/main/java/com/example/basicandroidapp/MainActivity.java
+++ app/src/main/java/com/example/basicandroidapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     EditText name, contact, dob;
     Button btnInsert, btnDelete, btnUpdate, btnView;
+    DBHelper db;
 
 
     @Override
@@ -27,20 +28,26 @@ public class MainActivity extends AppCompatActivity {
         btnDelete = findViewById(R.id.btnDelete);
         btnView = findViewById(R.id.btnView);
 
+        db = new DBHelper(this);
+
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String nameText = name.getText().toString();
                 String contactText = contact.getText().toString();
                 String dobText = dob.getText().toString();
+                Boolean insertedDataSuccessful = db.insertUserdata(nameText, contactText, dobText);
 
-
-                if (!nameText.isEmpty() && !contactText.isEmpty() & !dobText.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Inserted Successfully!!!", Toast.LENGTH_SHORT).show();
+                if (!nameText.isEmpty() && !contactText.isEmpty() & !dobText.isEmpty()){
+                    if(insertedDataSuccessful) {
+                        Toast.makeText(MainActivity.this, "Inserted Successfully!!!", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Inserted Failed!!!", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else{
-                    Toast.makeText(MainActivity.this, "Inserted Failed!!!", Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(MainActivity.this, "Please enter all inputs!!!", Toast.LENGTH_SHORT).show();
                 }
             }
             });
